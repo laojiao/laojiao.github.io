ef530f39904b55a6e2becd5164ed3bb2ec50416b
laojiao <lizhl@live.com>
2016-07-26 19:22:20 +0800

    Site updated: 2016-07-26 19:22:17

---
+++ js/timeline.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+             function movecolor(num){
+                 document.getElementById("l"+num).style.background = "#98E0FA" ;
+                 document.getElementById("backgroundimg").src="img/pic"+num+".jpg";
+             }
+             function outcolor(num){
+                 document.getElementById("l"+num).style.background = "#ccc" ;
+             }
+ </script>
